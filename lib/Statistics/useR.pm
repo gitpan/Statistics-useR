59dcea326c3f33c440daa6706f5f86ba5d9ce1ca
郑 鑫 <xinzheng@cpan.org>
2011-05-15 01:12:02 +0000

    Import of XINZHENG/Statistics-useR-0.1.0 from CPAN.
    gitpan-cpan-distribution: Statistics-useR
    gitpan-cpan-version:      0.1.0
    gitpan-cpan-path:         XINZHENG/Statistics-useR-0.1.0.tar.gz
    gitpan-cpan-author:       XINZHENG
    gitpan-cpan-maturity:     released

--- lib/Statistics/useR.pm
+++ lib/Statistics/useR.pm
@@ -1,25 +1,164 @@
+=head1 NAME
+
+Statistics::useR - Embed R to let Perl speak statistics. 
+
+=head1 SYNOPSIS
+
+	use Statistics::useR;
+
+=head1 DESCRIPTION
+
+The Statistics::useR is intended to integrate statistical power of R and comprehensive power of Perl. It defines a set of methods that make it possible to communicate easily between Perl and R. Furthermore, it offers a parallel environment for users to do multiple tasks at same time.
+
+Please keep in mind that Statistics::useR is currently just a layer of glue. User is assumed to know basic ideas of R.
+
+Statistics::useR can start an embedded R and evaluate R commands. It also offers an object-based interface to R data. The R data can be output into Perl (integer vector, real number vector and string vector, also lists containing such vectors) as a hash structure which is called RData. The hash contains array refs to data. Keys in hash are names of list in R. For vector data, though, hash with only value will be put back into Perl. It can also introduce a hash into R list data. In one word, useR exchanges data with R through Perl-hash/R-list. Please see details below.
+
+=head2 EXPORT
+
+Statistics::useR exports five methods to run embedded R.
+
+=cut
+
 package Statistics::useR;
 
 use 5.010001;
 use strict;
 use warnings;
+use XSLoader;
+use forks;
+use threads::shared;
+use Storable qw(store retrieve);
+use File::Temp qw(:POSIX);
+
+=head1 USAGE
+
+When this module is used, an R interpreter will be started automatically. It will also close by itself when the program ends. Following codes show how to evaluate R commands and how the RData looks like. It is important to understand the structure of RData. It is strongly suggested that you run the following two example codes.
+
+The first example could demonstrate how RData looks like.
+
+	use Statistics::useR;
+	use Data::Dumper;
+
+	my $cmd = 'l<-list(a=1:3,b=2:6,c=list(d=c(2,3.4,5.6),e=c(23.3,43.445),f=c(98.8,42.1,"df")));l';
+	my $list = eval_R($cmd);
+	print $list->getType(), "\n"; #get data type of cmd result
+	print $list->getLen(), "\n"; #get number of members of cmd result
+	print join "\t", @{$list->getNames()}; print "\n"; #get names(like 'names' in R)
+	print Dumper($list->getValue()), "\n"; #Dumped value will help understand how value is returned to Perl.
+
+	my $data = {'val',[1,4,3],'attr',['hello', 'world','universe']};
+	my $rvar = Statistics::RData->new('data'=>$data, 'name'=>'test'); #Set a new R data named 'test' in R.
+	$cmd='test2<-test;test2$val <- test2$val+5;pdf("pic.pdf");plot(test2$val);dev.off();test2';
+	my $res=eval_R($cmd);
+	print Dumper($res->getValue()), "\n";
+
+The second example shows how to share data and to run analysis in parallel way.
+
+	use Statistics::useR;
+
+The following assignment creates a shared data.
+	
+	my $res = eval_R('a<-rnorm(5600*5600);dim(a)<-c(5600,5600);b<-t(a);colnames(a)<-1:5600;as.data.frame(a)');
+
+Following codes create four tasks to calculate the matrix product of 'b' and 'a'.
+
+	my $th1 = openTask('as.data.frame(b %*% a[,1:1400])');
+	my $th2 = openTask('as.data.frame(b %*% a[,1401:2800])');
+	my $th3 = openTask('as.data.frame(b %*% a[,2801:4200])');
+	my $th4 = openTask('as.data.frame(b %*% a[,4201:5600])');
+
+Then close all the tasks. Please note that the tasks are opened together before get closed. Since B<closeTask> may block the main program, closing a task right after opening may actually equal to doing tasks sequentially.
+
+	my $o1 = closeTask($th1);
+	my $o2 = closeTask($th2);
+	my $o3 = closeTask($th3);
+	my $o4 = closeTask($th4);
+	my %res = (%{$o1},%{$o2},%{$o3},%{$o4}); #collect all returned RData.
+
+=cut
+
+BEGIN {
+  our $VERSION = '0.1';
+  XSLoader::load('Statistics::useR', $VERSION);
+  init_R();
+}
+END {
+  end_R();
+}
 
 require Exporter;
 
 our @ISA = qw(Exporter);
 
 our %EXPORT_TAGS = ( 'all' => [ qw() ],
-		     'runR' => [qw(init_R end_R eval_R)]
+		     'runR' => [qw(eval_R readTable writeTable openTask closeTask)]
     );
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = ( @{ $EXPORT_TAGS{'runR'} } );
 
-our $VERSION = '0.01';
+sub readTable {
+    my %data;
+    my @colnames;
+    open my $fh, '<', $_[0];
+    chomp(my $header = <$fh>);
+    @colnames = split /$_[1]/, $header;
+    for (@colnames) {
+      $data{$_} = [];
+    }
+    while(<$fh>) {
+      chomp;
+      my @eles = split /$_[1]/, $_;
+      for my $idx (0..$#eles) {
+	push @{$data{$colnames[$idx]}}, $eles[$idx];
+      }
+    }
+    close $fh;
+    return \%data;
+}
 
-require XSLoader;
-XSLoader::load('Statistics::useR', $VERSION);
+sub writeTable {
+    my ($data, $file, $delim) = @_;
+    my @headers = sort keys %$data;
+
+    open FH, '>', $file;
+    print FH join $delim, @headers;
+    print FH "\n";
+    for my $idx (0..$#{$data->{$headers[0]}}) {
+	print FH join $delim, map {$data->{$_}->[$idx]} @headers;
+	print FH "\n";
+    }
+    close FH;
+}
+
+sub openTask {
+  my $cmd = $_[0];
+  my $dataRepo = tmpnam();
+  my $t = threads->new(sub{
+			 my $out;
+			 if(eval {$out = eval_R($cmd)}) {
+			   store $out->getValue(), $dataRepo;
+			   return $dataRepo;
+			 }
+			 else {
+			   warn $@;
+			   return undef;
+			 }
+		       });
+ return $t;
+}
+
+sub closeTask {
+  my $dataRepo = $_[0]->join;
+  if(defined $dataRepo) {
+    my $dataRef = retrieve($dataRepo);
+    unlink $dataRepo;
+    return $dataRef;
+  }
+  else {return undef;}
+}
 
 package Statistics::RData;
 
@@ -27,13 +166,12 @@ use strict;
 use warnings;
 
 sub new {
-	shift;
-	my $input = {};
-	%{$input} = @_;
+    shift;
+    my $input = {};
+    %{$input} = @_;
     my %type = ('int',[],'real',[],'str',[]);;
-    my $keyno = keys(%{$input->{'data'}});
+    my $keyCount = keys(%{$input->{'data'}});
 
-#check data type    
     while(my ($key, $val) = each %{$input->{'data'}}) {
 	my $t1;
 	for (1..3) {
@@ -46,10 +184,10 @@ sub new {
 	push @{$type{$t1}}, $key;
     }
 
-    my $rdata = setvalue($input->{'data'}, \%type, $keyno);
-	insvar($rdata, $input->{'varname'});
+    my $rData = setValue($input->{'data'}, \%type, $keyCount);
+    insVar($rData, $input->{'name'});
 
-    return $rdata;
+    return $rData;
 }
 
 sub getDataType {
@@ -68,82 +206,71 @@ sub getDataType {
 }
 
 1;
+
 __END__
-
-=head1 NAME
-
-Statistics::useR - Embed R to let Perl speak statistics. 
-
-=head1 SYNOPSIS
-
-  use Statistics::useR;
-
-=head1 DESCRIPTION
-
-The Statistics::useR is intended to integrate statistical power of R and comprehensive power of Perl. It defines a set of methods that make it possible to communicate easily between Perl and R.
-
-Please keep in mind that Statistics::useR is currently just a layer of glue. User is assumed to know basic ideas of R.
-
-Statistics::useR can start an embedded R and evaluate R commands. It also offers an object-based interface to R data. The R data can be output into Perl (integer vector, real number vector and string vector, also lists containing such vectors) as a hash structure. The hash contains array refs to data. Keys in hash are names of list in R. For vector data, though, hash with only value will be put back into Perl. It can also introduce a hash into R list data. In one word, useR exchanges data with R through Perl-hash/R-list. Please see details below.
-
-=head2 EXPORT
-
-Statistics::useR exports three methods to run embedded R.
-
-=head1 USAGE
-
-	use Statistics::useR;
-	use Data::Dumper;
-
-	my $status = init_R; # init_R returns true if no error occurs. By default, R is inited with '--quiet' and '--vanilla'.
-	my $cmd = 'l<-list(a=1:3,b=2:6,c=list(d=c(2,3.4,5.6),e=c(23.3,43.445),f=c(98.8,42.1,"df")));l';
-	my $list = eval_R($cmd);
-	print $list->gettype(), "\n"; #get data type of cmd result
-	print $list->getlen(), "\n"; #get number of members of cmd result
-	print join "\t", @{$list->getnames()}; print "\n"; #get names(like 'names' in R)
-	print Dumper($list->getvalue()), "\n"; #Dumped value will help understand how value is returned to Perl.
-
-	my $data = {'val',[1,4,3],'attr',['hello', 'world','universe']};
-	my $rvar = Statistics::RData->new('data'=>$data, 'varname'=>'test'); #Set a new R data named 'test' in R.
-	$cmd='test2<-test;test2$val <- test2$val+5;pdf("pic.pdf");plot(test2$val);dev.off();test2';
-	my $res=eval_R($cmd);
-	print Dumper($res->getvalue()), "\n";
-
-	end_R(); #end R session.
 
 =head1 METHODS
 
 =over
 
-=item C<init_R>
+=item B<eval_R>
 
-Init R with default arguments - 'quiet' and 'vanilla'. 
+Evaluate R expression and return an object of RData. It is one way to share data between different tasks to evaluate an R command before creating any task by openTask. 
 
-=item C<end_R>
+ eval_R('a <- 1:100;');
+ my $task1 = openTask(...);
+ my $task2 = openTask(...);
+ #task1 and task2 both know data a.
 
-End R session.
+=item B<openTask>
 
-=item C<eval_R>
+Turn on a process to evaluate R commands. Given a scalar containing command string, a process object will be returned.
 
-Evaluate R expression and return an object of RData.
+ my $cmd = 'm <- rnomr(3*3); dim(m) <- c(3,3); crossprod(m)';
+ my $process = openTask($cmd);
 
-=item C<new>
+=item B<closeTask>
 
-Create a new R list - 'data' with name 'varname'. Return an object of RData. 'data' is a hash ref, which points to hash containing array refs. Created R list takes hash keys as names, and takes every array content as component data. Refer to usage.
+Turn off a process. Since it will block the main program, it is good to close tasks only after opening all needed tasks. Return an RData of the last evaluated expression in the R command.
 
-=item C<gettype>
+ my $res = closeTask($process);
+
+=item B<readTable>
+
+Read in a text file containing a table-style data. The data is assumed to have column names, yet without row names.
+
+ my $delim = "\t";
+ my $newdata = readTable 'filename.txt', $delim;
+
+=item B<writeTable>
+
+Put a RData into a text file in table format.
+ 
+ writeTable $RData, $filename, $delim;
+
+=back
+
+I<Following is Object-oriented methods for RData.>
+
+=over
+
+=item B<new>
+
+Create a new R list - 'data' with 'name'. Return an object of RData. 'data' is a hash ref, which points to hash containing array refs. Created R list takes hash keys as names, and takes every array content as component data. Refer to usage. And this is another way to share data between multiple tasks.
+
+=item B<getType>
 
 Get R data type.
 
-=item C<getlen>
+=item B<getLen>
 
 Get number of members in R data.
 
-=item C<getnames>
+=item B<getNames>
 
 Just like 'names' in R.
 
-=item C<getvalue>
+=item B<getValue>
 
 Return a hash ref which contains array refs and takes names of R list components as hash keys. The arrays map to components of R list.
 
@@ -152,19 +279,19 @@ Return a hash ref which contains array refs and takes names of R list components
 =head1 SEE ALSO
 
 R project website: http://www.r-project.org
-    
+
 =head1 AUTHOR
 
 Xin Zheng, E<lt>xinzheng@cpan.orgE<gt>
-Any feedback will be greatly appreciated.    
+Any feedback will be greatly appreciated.
+It may help if you go to http://david.abcc.ncifcrf.gov/R .
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010 by Xin Zheng
+Copyright (C) 2011 by LIB/SAIC-Frederick @ NCI-Frederick
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.10.1 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
--- useR.xs
+++ useR.xs
@@ -15,6 +15,8 @@
 #include <Rdefines.h>
 
 typedef struct VECTOR_SEXPREC RVec;
+extern uintptr_t R_CStackLimit;
+extern int R_Signalhandlers;
 
 //End R session
 IV end_R() {
@@ -24,19 +26,19 @@ IV end_R() {
 
 //Eval R command
 RVec * eval_R(char * cmd) {
-  SEXP cmdSexp, cmdexpr, ans = R_NilValue;
+  SEXP cmdSexp, cmdExpr, ans = R_NilValue;
   ParseStatus status;
   int i;
 
   PROTECT(cmdSexp = allocVector(STRSXP, 1));
   SET_STRING_ELT(cmdSexp, 0, mkChar(cmd));
-  cmdexpr = PROTECT(R_ParseVector(cmdSexp, -1, &status, R_NilValue));
+  cmdExpr = PROTECT(R_ParseVector(cmdSexp, -1, &status, R_NilValue));
   if (status != PARSE_OK) {
     UNPROTECT(2);
     error("invalie cmd %s", cmd);
   }
-  for(i=0; i<length(cmdexpr); i++) {
-    ans = eval(VECTOR_ELT(cmdexpr, i), R_GlobalEnv);
+  for(i=0; i<length(cmdExpr); i++) {
+    ans = eval(VECTOR_ELT(cmdExpr, i), R_GlobalEnv);
   }
   UNPROTECT(2);
   
@@ -44,7 +46,7 @@ RVec * eval_R(char * cmd) {
 }
 
 //Return the data type of R object
-const char * gettype(RVec * robj) {
+const char * getType(RVec * robj) {
   SEXPTYPE type = TYPEOF((SEXP)robj);
   switch(type) {
   case NILSXP:
@@ -142,7 +144,7 @@ AV * fetchSTR(RVec * robj) {
 }
 
 //Fetch object(intsxp, realsxp, strsxp, vecsxp) value. Return a hash storing ref of av.
-HV * fetchvalue(RVec * robj) {
+HV * fetchValue(RVec * robj) {
   HV * res = newHV();
   AV * arr;
   int len, i;
@@ -184,7 +186,7 @@ HV * fetchvalue(RVec * robj) {
 	    PUT_RV_2HV(res, name, (SV*)arr);
 	    break;
 	  case VECSXP:
-	    PUT_RV_2HV(res, name, (SV*)fetchvalue(v));
+	    PUT_RV_2HV(res, name, (SV*)fetchValue(v));
 	    break;
 	  default:
 	    hv_store(res, name, strlen(name), &PL_sv_undef, 0);
@@ -198,20 +200,20 @@ HV * fetchvalue(RVec * robj) {
   return res;
 }
 
-RVec * setINT(AV * intarr) {
-  RVec * intvec;
+RVec * setINT(AV * intArr) {
+  RVec * intVec;
   int i;
   SV ** mem;
 
-  PROTECT((SEXP)(intvec = (RVec*)allocVector(INTSXP, av_len(intarr)+1)));
-  for(i=0;i<av_len(intarr)+1;i++) {
-    mem = av_fetch(intarr, i, 0);
+  PROTECT((SEXP)(intVec = (RVec*)allocVector(INTSXP, av_len(intArr)+1)));
+  for(i=0;i<av_len(intArr)+1;i++) {
+    mem = av_fetch(intArr, i, 0);
     if(SvTYPE(*mem)==SVt_PVIV || SvTYPE(*mem)==SVt_IV || SvTYPE(*mem)==SVt_PV) {
-      INTEGER((SEXP)intvec)[i] = SvIV(*mem);
+      INTEGER((SEXP)intVec)[i] = SvIV(*mem);
       
     }
     else if(SvTYPE(*mem)==SVt_NULL) {
-      INTEGER((SEXP)intvec)[i] = NA_INTEGER;
+      INTEGER((SEXP)intVec)[i] = NA_INTEGER;
     }
     else {
       croak("Data No.%d is not integer number", i+1);
@@ -219,22 +221,22 @@ RVec * setINT(AV * intarr) {
   }
   UNPROTECT(1);
 
-  return intvec;
+  return intVec;
 }
 
-RVec * setREAL(AV * realarr) {
-  RVec * realvec;
+RVec * setREAL(AV * realArr) {
+  RVec * realVec;
   int i;
   SV ** mem;
 
-  PROTECT((SEXP)(realvec = (RVec*)allocVector(REALSXP, av_len(realarr)+1)));
-  for(i=0;i<av_len(realarr)+1;i++) {
-    mem = av_fetch(realarr, i, 0);
+  PROTECT((SEXP)(realVec = (RVec*)allocVector(REALSXP, av_len(realArr)+1)));
+  for(i=0;i<av_len(realArr)+1;i++) {
+    mem = av_fetch(realArr, i, 0);
     if(SvTYPE(*mem)==SVt_PVNV || SvTYPE(*mem)==SVt_NV || SvTYPE(*mem)==SVt_PV) {
-      REAL((SEXP)realvec)[i] = SvNV(*mem);
+      REAL((SEXP)realVec)[i] = SvNV(*mem);
     }
     else if(SvTYPE(*mem)==SVt_NULL) {
-      REAL((SEXP)realvec)[i] = NA_REAL;
+      REAL((SEXP)realVec)[i] = NA_REAL;
     }
     else {
       croak("Data No.%d is not real number", i+1);
@@ -242,23 +244,23 @@ RVec * setREAL(AV * realarr) {
   }
   UNPROTECT(1);
 
-  return realvec;
+  return realVec;
 }
 
-RVec * setSTR(AV * strarr) {
-  RVec * strvec;
+RVec * setSTR(AV * strArr) {
+  RVec * strVec;
   int i;
   SV ** mem;
 
-  PROTECT((SEXP)(strvec = (RVec*)allocVector(STRSXP, av_len(strarr)+1)));
-  for(i=0;i<av_len(strarr)+1;i++) {
-    mem = av_fetch(strarr, i, 0);
+  PROTECT((SEXP)(strVec = (RVec*)allocVector(STRSXP, av_len(strArr)+1)));
+  for(i=0;i<av_len(strArr)+1;i++) {
+    mem = av_fetch(strArr, i, 0);
     if(SvTYPE(*mem)==SVt_PV || SvTYPE(*mem) == SVt_PVIV || SVt_PVNV) {
-      SET_STRING_ELT((SEXP)strvec, i, mkChar(SvPV_nolen(*mem)));
+      SET_STRING_ELT((SEXP)strVec, i, mkChar(SvPV_nolen(*mem)));
       
     }
     else if(SvTYPE(*mem)==SVt_NULL){
-      SET_STRING_ELT((SEXP)strvec, i, NA_STRING);
+      SET_STRING_ELT((SEXP)strVec, i, NA_STRING);
     }
     else {
       croak("Data No.%d is not string", i+1);
@@ -266,7 +268,7 @@ RVec * setSTR(AV * strarr) {
   }
   UNPROTECT(1);
   
-  return strvec;
+  return strVec;
 }
 
 MODULE = Statistics::useR		PACKAGE = Statistics::useR
@@ -276,10 +278,12 @@ init_R(optc=0,opts=NULL)
   int optc;
   char ** opts;
  PREINIT:
-  char * defargv[] = {"REmbedded", "--quiet", "--vanilla"};
+  char * defArgv[] = {"REmbedded", "--quiet", "--vanilla"};
  CODE:
-  if(opts==NULL){opts = defargv; optc = sizeof(defargv)/sizeof(char*);}
+  if(opts==NULL){opts = defArgv; optc = sizeof(defArgv)/sizeof(char*);}
+  R_SignalHandlers = 0;
   RETVAL = Rf_initEmbeddedR(optc, opts);
+  R_CStackLimit = -1;
  OUTPUT:
   RETVAL
 
@@ -292,79 +296,79 @@ end_R()
 MODULE = Statistics::useR		PACKAGE = Statistics::RData
 
 const char *
-gettype(robj)
-  RVec * robj;
+getType(rObj)
+  RVec * rObj;
 
 IV
-getlen(robj)
-  RVec * robj;
+getLen(rObj)
+  RVec * rObj;
  CODE:
-  RETVAL = LENGTH((SEXP)robj);
+  RETVAL = LENGTH((SEXP)rObj);
  OUTPUT:
   RETVAL
 
 AV *
-getnames(robj)
-  RVec * robj;
+getNames(rObj)
+  RVec * rObj;
  PREINIT:
   RVec * names;
-  AV * nameav;
+  AV * nameAV;
   int i;
  CODE:
-  names = (RVec*)GET_NAMES((SEXP)robj);
-  nameav = (AV*)sv_2mortal((SV*)newAV());
+  names = (RVec*)GET_NAMES((SEXP)rObj);
+  nameAV = (AV*)sv_2mortal((SV*)newAV());
   for(i=0;i<LENGTH((SEXP)names);i++) {
-    av_push(nameav, newSVpv((CHAR(STRING_ELT((SEXP)names,i))), 0));
+    av_push(nameAV, newSVpv((CHAR(STRING_ELT((SEXP)names,i))), 0));
   }
-  RETVAL = nameav;
+  RETVAL = nameAV;
  OUTPUT:
   RETVAL
 
 HV *
-getdimnames(robj)
-  RVec * robj;
+getDimNames(rObj)
+  RVec * rObj;
  PREINIT:
-  HV * namehv;
-  AV * nameav;
-  RVec * dimnames;
+  HV * nameHV;
+  AV * nameAV;
+  RVec * dimNames;
   RVec * names;
   int i,j;
   char name[2];
  CODE:
-  namehv = (HV*)sv_2mortal((SV*)newHV());
-  dimnames = (RVec*)GET_DIMNAMES((SEXP)robj);
-  if(TYPEOF((SEXP)dimnames)!=VECSXP) {
-   namehv=(HV*)&PL_sv_undef;
+  nameHV = (HV*)sv_2mortal((SV*)newHV());
+  dimNames = (RVec*)GET_DIMNAMES((SEXP)rObj);
+  if(TYPEOF((SEXP)dimNames)!=VECSXP) {
+   nameHV=(HV*)&PL_sv_undef;
   }
   else {
-    for(i=0;i<LENGTH((SEXP)dimnames);i++) {
-      nameav = newAV();
-      names = (RVec*)VECTOR_ELT((SEXP)dimnames,i);
+    for(i=0;i<LENGTH((SEXP)dimNames);i++) {
+      nameAV = newAV();
+      names = (RVec*)VECTOR_ELT((SEXP)dimNames,i);
       for(j=0;j<LENGTH((SEXP)names);j++) {
-        av_push(nameav, newSVpv((CHAR(STRING_ELT((SEXP)names,j))), 0));
+        av_push(nameAV, newSVpv((CHAR(STRING_ELT((SEXP)names,j))), 0));
       }
     sprintf(name, "%c", i+48);
-    hv_store(namehv, name, strlen(name), newRV_noinc((SV*)nameav), 0);
+    hv_store(nameHV, name, strlen(name), newRV_noinc((SV*)nameAV), 0);
     }
   }
-  RETVAL = namehv;
+  RETVAL = nameHV;
  OUTPUT:
   RETVAL
 
 
 HV *
-getvalue(robj)
-  RVec * robj;
+getValue(rObj)
+  RVec * rObj;
  CODE:
-  RETVAL = (HV*)sv_2mortal((SV*)fetchvalue(robj));
+  RETVAL = (HV*)sv_2mortal((SV*)fetchValue(rObj));
  OUTPUT:
   RETVAL
 
 RVec *
-setvalue(datahv, typehv, keyno)
-  HV * datahv;
-  HV * typehv; //Contains 'int', 'real', 'str'
-  IV keyno;
+setValue(dataHV, typeHV, keyCount)
+  HV * dataHV;
+  HV * typeHV; //Contains 'int', 'real', 'str'
+  IV keyCount;
  PREINIT:
   RVec * res;
   RVec * names;
@@ -374,36 +378,36 @@ setvalue(datahv, typehv, keyno)
   char * key;
  CODE:
   if(SvROK(ST(0)) && SvTYPE(SvRV(ST(0)))==SVt_PVHV) {
-    datahv = (HV*)SvRV(ST(0));
+    dataHV = (HV*)SvRV(ST(0));
   }
   else {
     croak("First argument is not hash ref");
   }
-  typehv = (HV*)SvRV(ST(1));
-  keyno = SvIV(ST(2)); 
-  PROTECT((SEXP)(res = (RVec*)allocVector(VECSXP, keyno)));
-  PROTECT((SEXP)(names = (RVec*)allocVector(STRSXP, keyno)));
-  keys = (AV*)(SvRV(*(hv_fetch(typehv, "str", 3, 0))));
+  typeHV = (HV*)SvRV(ST(1));
+  keyCount = SvIV(ST(2)); 
+  PROTECT((SEXP)(res = (RVec*)allocVector(VECSXP, keyCount)));
+  PROTECT((SEXP)(names = (RVec*)allocVector(STRSXP, keyCount)));
+  keys = (AV*)(SvRV(*(hv_fetch(typeHV, "str", 3, 0))));
   for(i=0;i<=av_len(keys);i++) {
     key = SvPV_nolen(*(av_fetch(keys, i, 0)));
     SET_STRING_ELT((SEXP)names, i, mkChar(key));
-    colvalue = (AV*)(SvRV(*(hv_fetch(datahv, key, strlen(key),0))));
+    colvalue = (AV*)(SvRV(*(hv_fetch(dataHV, key, strlen(key),0))));
     SET_VECTOR_ELT((SEXP)res, i, (SEXP)setSTR(colvalue));
   }
   i = av_len(keys);
-  keys = (AV*)(SvRV(*(hv_fetch(typehv, "int", 3, 0))));
+  keys = (AV*)(SvRV(*(hv_fetch(typeHV, "int", 3, 0))));
   for(j=0;j<=av_len(keys);j++) {
     key = SvPV_nolen(*(av_fetch(keys, j, 0)));
     SET_STRING_ELT((SEXP)names, (i+j+1), mkChar(key));
-    colvalue = (AV*)(SvRV(*(hv_fetch(datahv, key, strlen(key),0))));
+    colvalue = (AV*)(SvRV(*(hv_fetch(dataHV, key, strlen(key),0))));
     SET_VECTOR_ELT((SEXP)res, (i+j+1), (SEXP)setINT(colvalue));
   }
   j = av_len(keys);
-  keys = (AV*)(SvRV(*(hv_fetch(typehv, "real", 4, 0))));
+  keys = (AV*)(SvRV(*(hv_fetch(typeHV, "real", 4, 0))));
   for(k=0;k<=av_len(keys);k++) {
     key = SvPV_nolen(*(av_fetch(keys, k, 0)));
     SET_STRING_ELT((SEXP)names, (i+j+k+2), mkChar(key));
-    colvalue = (AV*)(SvRV(*(hv_fetch(datahv, key, strlen(key),0))));
+    colvalue = (AV*)(SvRV(*(hv_fetch(dataHV, key, strlen(key),0))));
     SET_VECTOR_ELT((SEXP)res, (i+j+k+2), (SEXP)setREAL(colvalue));
   }
   SET_NAMES((SEXP)res, (SEXP)names);
@@ -413,9 +417,9 @@ setvalue(datahv, typehv, keyno)
   RETVAL
 
 void
-insvar(var, varname)
+insVar(var, varName)
   RVec * var;
-  char * varname;
+  char * varName;
  CODE:
-  defineVar(install(varname), (SEXP)var, R_GlobalEnv);
+  defineVar(install(varName), (SEXP)var, R_GlobalEnv);
   
